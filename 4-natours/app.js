b50691bfc824f03ab95fa4736d4ade532a83ebd3
Your Name <you@example.com>
2025-05-20 19:25:35 +0300

    Created Tour Routes and Routes Handlers for GET,POST,PATCH,DELETE and used body parsing middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,8 +1,79 @@
+const fs=require('fs')
 const express=require('express')
 const app=express()
+app.use(express.json())
+const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get('/',(req,res)=>{
- res.send("hello from server")
+app.get('/api/v1/tours',(req,res)=>{
+ res.status(200).json({
+    status:"success",
+    results:tours.length,
+    data:{
+    tours
+}
+})
+})
+
+app.post('/api/v1/tours',(req,res,)=>{
+    const newId=tours[tours.length-1].id +1
+    const newTour=Object.assign({id:newId},req.body)
+    console.log(newTour)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
+           res.status(201).json({
+        status:'success',
+        data:{
+            tour:newTour
+        }
+    })
+    })
+   
+})
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+ if(req.params.id*1>tours.length){
+    return res.status(404).json({
+        status:'Fail',
+        message:'Invalid Id'
+    })
+ }
+ const tour=tours.find(tour=>tour.id===req.params.id*1)
+ res.status(200).json({
+    status:'success',
+    data:{
+        tour
+    }
+ })
+})
+
+app.patch('/api/v1/tours/:id',(req,res)=>{
+     if(req.params.id*1>tours.length){
+    return res.status(404).json({
+        status:'Fail',
+        message:'Invalid Id'
+    })
+ }
+
+ res.status(201).json({
+    status:'success',
+    data:{
+        tour:'updated tour'
+    }
+ })
+})
+
+app.delete('/api/v1/tours/:id',(req,res)=>{
+     if(req.params.id*1>tours.length){
+    return res.status(404).json({
+        status:'Fail',
+        message:'Invalid Id'
+    })
+ }
+
+ res.status(204).json({
+    status:'success',
+    data:null
+ })
 })
 
 const port=3000
