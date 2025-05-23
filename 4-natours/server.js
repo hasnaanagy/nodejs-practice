2196a2332517cff9e4025db2029823da20fc3724
Your Name <you@example.com>
2025-05-23 03:24:52 +0300

    created first document from node app and save it to our remote database

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -35,6 +35,18 @@ const tourSchema=mongoose.Schema({
 })
 
 const Tour=mongoose.model('Tour',tourSchema)
+const tour1=new Tour({
+    name:'Tour 1',
+    rating:3.5,
+    price:100
+})
+
+tour1.save().then(doc=>{
+    console.log(doc)
+}).catch(err=>{
+    console.log(err)
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log('Listening on port 3000 ...');
