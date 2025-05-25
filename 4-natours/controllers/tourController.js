6ddf79cc735022b168e7996f554bb50cb9322579
Your Name <you@example.com>
2025-05-25 08:07:55 +0300

    Made getAllTours API better by using filtering,sorting,projection and pagination

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,23 +1,72 @@
 const fs = require('fs');
-const Tour=require('../models/tourModel')
+const Tour=require('../models/tourModel');
 
 
 exports.getTours =async (req, res) => {
 try{
- const tours=await Tour.find()
- res.status(200).json({
-    status:'success',
+    //BUILD QUERY
+    console.log(req.query)
+  //1- execlude[page,sort,limit,field] from query string
+  const queryObj={...req.query}
+  const execludedFields=["page","sort","limit","field"]
+  execludedFields.forEach(field=>delete queryObj[field])
+
+  //2- replace filtering operators with $opertor
+  let queryString=JSON.stringify(queryObj)
+  queryString=queryString.replace(/\b(gte|gt|lt|lte)\b/g,match=>`$${match}`)
+  let query=Tour.find(JSON.parse(queryString))
+
+  //3-Sorting 
+
+  if(req.query.sort){
+    const sortBy=req.query.sort.split(',').join(' ')
+    query=query.sort(sortBy)
+  }else{
+    query=query.sort('-createdAt')
+  }
+
+  //4-Projection (limiting fields)
+
+  if(req.query.field){
+    const fields=req.query.field.split(',').join(' ')
+    query=query.select(fields)
+  }else{
+        query=query.select('-__v')
+  }
+
+  //pagination (page+limit)
+  const page=req.query.page*1||1
+  const limit=req.query.limit*1||100
+  const skip=(page-1)*limit
+
+query=query.skip(skip).limit(limit)
+
+//in case user request a page that does not exist
+
+if(req.query.page){
+    const numOfTours=await Tour.countDocuments()
+    if(skip>=numOfTours)
+        throw new Error('Page Not Exist')
+}
+
+ //EXECUTE QUERY
+  const tours=await query
+
+res.status(200).json({
+    status:"success",
+    results:tours.length,
     data:{
         tours
     }
- })
+})
 }catch(err){
- res.status(400).json({
-    status:'fail',
-    message:err
- })
+    res.status(400).json({
+        status:'fail',
+        message:err
+    })
 }
 };
+
 
 exports.createTour =async (req, res) => {
 try{
