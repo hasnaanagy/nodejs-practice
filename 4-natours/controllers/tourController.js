afd3185ab76c60c2f80d4ae0c2808cbe50b4adb2
Your Name <you@example.com>
2025-05-25 09:26:51 +0300

    Refactored controllers by creating an APIFeaturs class

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
 const Tour=require('../models/tourModel');
-
+const APIFeaturs=require('../utils/apiFeatures')
 exports.top5Cheap=(req,res,next)=>{
     req.query.limit="5"
     req.query.sort='-ratingsAverage,price'
@@ -8,55 +7,12 @@ exports.top5Cheap=(req,res,next)=>{
     next()
 }
 
+
 exports.getTours =async (req, res) => {
 try{
-    //BUILD QUERY
-    console.log(req.query)
-  //1- execlude[page,sort,limit,field] from query string
-  const queryObj={...req.query}
-  const execludedFields=["page","sort","limit","field"]
-  execludedFields.forEach(field=>delete queryObj[field])
 
-  //2- replace filtering operators with $opertor
-  let queryString=JSON.stringify(queryObj)
-  queryString=queryString.replace(/\b(gte|gt|lt|lte)\b/g,match=>`$${match}`)
-  let query=Tour.find(JSON.parse(queryString))
-
-  //3-Sorting 
-
-  if(req.query.sort){
-    const sortBy=req.query.sort.split(',').join(' ')
-    query=query.sort(sortBy)
-  }else{
-    query=query.sort('-createdAt')
-  }
-
-  //4-Projection (limiting fields)
-
-  if(req.query.field){
-    const fields=req.query.field.split(',').join(' ')
-    query=query.select(fields)
-  }else{
-        query=query.select('-__v')
-  }
-
-  //pagination (page+limit)
-  const page=req.query.page*1||1
-  const limit=req.query.limit*1||100
-  const skip=(page-1)*limit
-
-query=query.skip(skip).limit(limit)
-
-//in case user request a page that does not exist
-
-if(req.query.page){
-    const numOfTours=await Tour.countDocuments()
-    if(skip>=numOfTours)
-        throw new Error('Page Not Exist')
-}
-
- //EXECUTE QUERY
-  const tours=await query
+ const featurs=new APIFeaturs(Tour.find(),req.query).filter().sort().limitFields().paginate()
+  const tours=await featurs.query
 
 res.status(200).json({
     status:"success",
